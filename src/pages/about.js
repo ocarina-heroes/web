c04e41e60e44f1ddff40d6d54128ae76c2e0226b
Hrusikesh Panda <hrusikesh.panda@dowjones.com>
2020-02-29 23:38:03 -0500

    chore: Move into src folder

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function About() {
+  return (
+    <div className="container">
+      <Head>
+        <title>About Ocarina Heroes</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">Welcome to Ocarina Heroes!</h1>
+
+        <p className="description">This is the about page</p>
+      </main>
+    </div>
+  )
+}
